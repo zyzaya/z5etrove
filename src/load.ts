b2619611f60703a3d75b46a72c77c91e502d8acd
Riley Sherlock <riley.sherlock@mts.net>
2024-12-28 11:56:31 -0600

    implement csv loading

---
+++ src/load.ts
@@ -0,0 +1,15 @@
+// import * as csv from "csv-parser";
+// import * as fs from "fs";
+
+// let results: any = [];
+
+// fs.createReadStream('data.csv')
+//   .pipe(csv())
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     console.log(results);
+//     // [
+//     //   { NAME: 'Daffy Duck', AGE: '24' },
+//     //   { NAME: 'Bugs Bunny', AGE: '22' }
+//     // ]
+//   });
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,51 @@
 import './style.css'
 import './zgrid_default.css'
 import { zGrid } from './zGrid.js'
+import Papa from "papaparse";
+
+let results: any = [];
 
 let container = document.getElementById("zgrid_container")
+let grid = new zGrid({
+  parent: document.createElement('div'),
+  data: [],
+  columns: [
+    "Name",
+    "Source",
+    "Cost",
+    "Attunement",
+    "Rarity",
+    "Include?",
+    "Reason",
+    "Quantity",
+    "Text"
+  ],
+  pagination: 20,
+})
 
 if (container != null) {
-  let grid = new zGrid({
-    parent: container,
-    data: [],
-    columns: [
-      "Name", 
-      "Source", 
-      "Cost", 
-      "Attunement", 
-      "Rarity",
-      "Include?",
-      "Reason",
-      "Quantity"
-    ]
+  grid.reconfigure({
+    parent: container
   })
 }
+
+let file = document.getElementById("load_file")
+if (file != null) {
+  file.addEventListener('change', (event) => {
+    if (event.target == null) return;
+    Papa.parse(event.target.files[0], {
+      header: true,
+      complete: (results) => {
+        console.log(results)
+        let data = []
+        results.data.forEach((e, i) => {
+          let row = e;
+          row["id"] = i;
+          data.push(row)
+        })
+        grid.reconfigure({ data: data })
+      }
+    })
+  })
+}
+

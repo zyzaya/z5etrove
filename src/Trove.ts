088b3a825f53fd7ee85913cd2eaed7d78ac51400
Riley Sherlock <riley.sherlock@mts.net>
2024-12-30 11:23:02 -0600

    create display for trove settings

---
+++ src/Trove.ts
@@ -0,0 +1,13 @@
+let rarities = [
+  "common",
+  "uncommon",
+  "rare",
+  "very rare",
+  "legendary",
+  "artifact",
+]
+
+export default class Trove {
+  constructor(parent: HTMLDivElement) {
+  }
+}
